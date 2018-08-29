57aebd413f19267df98227895ad11a95296cb680
Chris <cny3123@gmail.com>
2018-08-29 00:15:08 -0700

    Add rspec homework

--- W2D3/rspec/spec/dessert_spec.rb
+++ W2D3/rspec/spec/dessert_spec.rb
@@ -6,16 +6,25 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "Tucker") }
+  let(:brownie) { Dessert.new("brownie", 100, chef) }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(brownie.type).to eq("brownie")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(100)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "tons", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
